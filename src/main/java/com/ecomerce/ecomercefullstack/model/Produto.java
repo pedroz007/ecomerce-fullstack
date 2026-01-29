464b21fb72c1a95e1e53c7b94c00933410712f59
pedroz007 <ph5987032@gmail.com>
2026-01-29 09:17:38 -0300

    banco em memória configurado

--- src/main/java/com/ecomerce/ecomercefullstack/model/Produto.java
+++ src/main/java/com/ecomerce/ecomercefullstack/model/Produto.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -30,7 +32,8 @@ public class Produto {
     @Column(nullable = false)
     private double preco;
 
-    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
     
 }
